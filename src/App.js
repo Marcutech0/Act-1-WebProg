4f3ab0b40f96fc6d7e1a0a5cb925429082738eba
Josh Guinto <jmpheen@gmail.com>
2024-09-11 20:26:37 +0800

    revised

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ function App() {
   return (
     <div className="container">
     <div className="box">
-      <UserProfiles name="John Doe" age="30" location="New York" />
+      <UserProfiles name="John Doe" age="30" location="New York" image="https://i.pinimg.com/564x/0d/64/98/0d64989794b1a4c9d89bff571d3d5842.jpg"/>
     </div>
     <div className="box">
-      <UserProfiles name="Jane Smith" age="25" location="San Francisco" />
+      <UserProfiles name="Jane Doe" age="25" location="San Francisco" image="https://i.pinimg.com/564x/0d/64/98/0d64989794b1a4c9d89bff571d3d5842.jpg"/>
     </div>
     <div className="box">
-      <UserProfiles name="Josh Guinto" age="21" location="Antipolo" />
+      <UserProfiles name="Josh Guinto" age="21" location="Antipolo" image="https://i.pinimg.com/564x/0d/64/98/0d64989794b1a4c9d89bff571d3d5842.jpg"/>
     </div>
   </div>
   );
--- src/UserProfile.js
+++ src/UserProfile.js
@@ -3,9 +3,11 @@ import React from "react";
 function UserProfiles(props) {
     return (
     <div className="App">
+       <img src={props.image} alt={`${props.name}'profilePic`} className="profilePicture" />
       <h1>{props.name}</h1>
       <p>Age: {props.age}</p>
       <p>Location: {props.location}</p>
+      <button className="dummyButton">View Profile</button>
   </div>
     );
 }
